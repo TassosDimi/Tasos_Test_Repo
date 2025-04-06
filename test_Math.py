3bb5a736202befbc6539914a4babb870f1560eb2
Tasos Dimitriou <danastassios@hotmail.com>
2025-04-06 13:37:01 +0100

    Adding test_Math.py file in my local

---
+++ test_Math.py
@@ -0,0 +1,7 @@
+"""This is just a test file for testing branches in Git and GitHub"""
+
+print("This is line 1.")
+print("This is line 2.")
+print("This is line 3.")
+print("This is line 4")
+print("\nThis a new line, line 5")
